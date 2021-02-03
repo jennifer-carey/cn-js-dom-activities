87e50d62db9817e419d18c48203616bd8f8350c9
Jennifer <jennifacarey@gmail.com>
2021-02-03 10:48:55 +0000

    #6 corrected event listener to match variable name

--- activity-6/app.js
+++ activity-6/app.js
@@ -6,6 +6,6 @@ const image = document.getElementById("image");
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
 
-submit.addEventListener("click", () => {
+button.addEventListener("click", () => {
    image.src = input.value;
 })
