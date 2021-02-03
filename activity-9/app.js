88e84407d612ff9a395a643056feac8d6343b4f0
Jennifer <jennifacarey@gmail.com>
2021-02-03 10:08:42 +0000

    #9 create variable for remove button

--- activity-9/app.js
+++ activity-9/app.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
 const list = document.querySelector("#list");
+const removebtn = document.querySelector("#remove");
 
 button.addEventListener("click", () => {
    let listItem = document.createElement("li"); 
