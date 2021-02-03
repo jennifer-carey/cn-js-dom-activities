3c5062e036e3f59f67d50d661f4fabeb62f5de03
Jennifer <jennifacarey@gmail.com>
2021-02-03 09:14:19 +0000

    #8 create function to update list

--- activity-8/app.js
+++ activity-8/app.js
@@ -1,3 +1,10 @@
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
+const list = document.querySelector("#list");
 
+submit.addEventListener("click", () => {
+   let listItem = document.createElement("li"); 
+   listItem.textContent = input.value;
+   //console.log(listItem);
+   list.appendChild(listItem);
+})
