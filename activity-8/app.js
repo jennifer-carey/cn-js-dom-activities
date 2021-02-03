ff8eb216fba14aea33889445a7b4708bae6ddfeb
Jennifer <jennifacarey@gmail.com>
2021-02-03 09:34:12 +0000

    #8 add variable for hideshow button in js

--- activity-8/app.js
+++ activity-8/app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
 const list = document.querySelector("#list");
+const hideshow-btn = document.querySelector("#hideshow-btn");
 
 submit.addEventListener("click", () => {
    let listItem = document.createElement("li"); 
